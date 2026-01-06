8e0e3de0cf576e1fcf2de6664a95d6844aeab755
neilhdezs <hernandezsalvadorneil@gmail.com>
2026-01-06 21:30:46 +0100

    añadida base de datos común para todos

--- src/main/java/com/lorenakl/login/controller/LoginController.java
+++ src/main/java/com/lorenakl/login/controller/LoginController.java
@@ -1,7 +1,20 @@
 package com.lorenakl.login.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+
+    public ResponseEntity<String> info() {
+        return ResponseEntity.ok("");
+    } // 0, 100, 200, 300, 400, 500
+    // 404 -> Cliente
+
+
+    public ResponseEntity<String> login(String username, String password) {
+        return ResponseEntity.ok("");
+    }
+
+
 }
